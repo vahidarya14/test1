065deaf1959dd6546df818a83403805d45d51d8b
Vahid Arya <vahidarya14@gmail.com>
2024-08-02 18:25:17 +0330

    imple state detect

--- BackendTest/Application/FlightService.cs
+++ BackendTest/Application/FlightService.cs
@@ -1,6 +1,7 @@
 ﻿using Domain;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Database;
+using System.ComponentModel.Design.Serialization;
 
 namespace ConsoleApp1.Core.Application;
 
@@ -8,21 +9,66 @@ public class FlightService(AppDbContext db)
 {
     public async Task<List<SearchResult>> Search(DateOnly startDate, DateOnly endDate, int agencyId)
     {
+
         var res = from route in db.routes
                   join flight in db.flights on route.route_id equals flight.route_id
-                  join s in db.subscriptions on route.origin_city_id equals s.origin_city_id
-                  where s.agency_id == agencyId && route.departure_date >= startDate && route.departure_date <= endDate
+                  join subscr in db.subscriptions on route.origin_city_id equals subscr.origin_city_id
+                  where subscr.agency_id == agencyId && route.departure_date >= startDate && route.departure_date <= endDate
                   select new SearchResult
                   {
                       airline_id = flight.airline_id,
-                      arrival_time=flight.arrival_time,    
-                      departure_time=flight.departure_time,
-                      flight_id=flight.flight_id,
-                      destination_city_id=s.destination_city_id,
-                      origin_city_id=s.origin_city_id,
-                      status="New"
+                      arrival_time = flight.arrival_time,
+                      departure_time = flight.departure_time,
+                      flight_id = flight.flight_id,
+                      destination_city_id = subscr.destination_city_id,
+                      origin_city_id = subscr.origin_city_id,
+                      status = "",
                   };
-        return await res.ToListAsync();
+        var lst = await res.OrderBy(x => x.departure_time).ToListAsync();
+
+        await DetectState( lst);
+
+        return lst;
+    }
+
+    private  async Task DetectState( List<SearchResult> lst)
+    {
+        var sevenDaysPlusMinuts = 10080 + 30;//7days + 30 minutes
+        var sevenDaysMinMinuts = 10080 - 30; //7days - 30 minutes
+
+        //load base date from database
+        var allflights = await db.flights
+            .Select(x => new
+            {
+                x.airline_id,
+                discontinued_max = ((DateTimeOffset)x.departure_time.AddMinutes(sevenDaysPlusMinuts)).ToUnixTimeSeconds(),
+                discontinued_min = ((DateTimeOffset)x.departure_time.AddMinutes(sevenDaysMinMinuts)).ToUnixTimeSeconds(),
+                new_max = ((DateTimeOffset)x.departure_time.AddMinutes(-sevenDaysPlusMinuts)).ToUnixTimeSeconds(),
+                new_min = ((DateTimeOffset)x.departure_time.AddMinutes(-sevenDaysMinMinuts)).ToUnixTimeSeconds()
+            })
+            .ToListAsync();
+
+
+        for (var i = 0; i < lst.Count; i++)
+        {
+            //a new flight if no flight from same airline exists with departure time T’ = T - 7 days (+/- 30 minutes tolerance).
+            var f = allflights.Any(x => lst[i].airline_id == x.airline_id &&
+                                       lst[i].dt >= x.discontinued_min &&
+                                       lst[i].dt <= x.discontinued_max);
+            if (!f)
+            {
+                lst[i].status = "new";
+            }
+
+            //discontinued if no flight from same airline exists with departure time T’ = T + 7 days (+/- 30 minutes tolerance)
+            f = allflights.Any(x => lst[i].airline_id == x.airline_id &&
+                                    lst[i].dt >= x.discontinued_min &&
+                                    lst[i].dt <= x.discontinued_max);
+            if (!f)
+            {
+                lst[i].status = "discontinued";
+            }
+        }
     }
 }
 
@@ -36,4 +82,9 @@ public class SearchResult
     public DateTime departure_time { get; set; }
     public DateTime arrival_time { get; set; }
     public string status { get; set; }// New / Discontinued
+
+    public DateTime next_max_departure_time { get; set; }
+    public DateTime next_min_departure_time { get; set; }
+
+    public long dt => ((DateTimeOffset)departure_time).ToUnixTimeSeconds();
 }
